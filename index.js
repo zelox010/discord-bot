b2dead7b02ae6fdb6c729bbce987ca8f9804e8bf
Zelox <121449127+zelox010@users.noreply.github.com>
2025-03-17 02:05:29 +0530

    Create index.js
    discord bot main core

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();  // Loads environment variables
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN); // Uses an environment variable for security
